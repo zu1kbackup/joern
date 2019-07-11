9af2885ea405456f619ae85a4c07b25bfca00994
Fabian Yamaguchi <fabs@shiftleft.io>
2019-07-11 17:31:28 +0200

    Tests for `ast` and `children`.

--- joern-cli/src/test/scala/io/shiftleft/joern/MethodBodyTests.scala
+++ joern-cli/src/test/scala/io/shiftleft/joern/MethodBodyTests.scala
@@ -1,3 +1,33 @@
 package io.shiftleft.joern
 
-class MethodBodyTests {}
+import org.scalatest.{Matchers, WordSpec}
+import io.shiftleft.queryprimitives.steps.Implicits._
+
+class MethodBodyTests extends WordSpec with Matchers {
+
+  val code = """
+                int foo(int x) {
+                  if (x > 10) {
+                    return 10;
+                  } else {
+                    return x;
+                  }
+                }
+             """.stripMargin
+
+  "should find AST for method and children of body" in
+    new TestCpg().buildCpg(code) { cpg =>
+      val numAstNodes = cpg.method.body.map(_.ast.l.size).l
+      val numChildren = cpg.method.body.map(_.children.l.size).l
+      numAstNodes.map(_ - 1) shouldBe numChildren
+    }
+
+  "should find blocks" in
+  new TestCpg().buildCpg(code) { cpg =>
+    cpg.method.body.children.block.map(_.children.l.map(_.getClass.getSimpleName)).l shouldBe
+      List(List("Return", "Literal"), List("Return", "Identifier"))
+  }
+
+
+
+}
